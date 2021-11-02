d8ad46a6dd9753ff7ba758ade65165bcc132186a
VSarabai <sarabai321@gmail.com>
2021-11-02 18:43:25 +0200

    update

---
+++ src/redux/actions/cart.js
@@ -0,0 +1,21 @@
+export const addPizzaToCart = (pizzaObg) => ({
+    type: 'ADD_PIZZA_CART',
+    payload: pizzaObg
+})
+
+export const clearCart = () => ({
+    type: 'CLEAR_CART',
+})
+export const removeCartItem = (id) => ({
+    type: 'REMOVE_CART_ITEM',
+    payload: id
+})
+
+export const plusCartItem = (id) => ({
+    type: 'PLUS_CART_ITEM',
+    payload: id
+})
+export const minusCartItem = (id) => ({
+    type: 'MINUS_CART_ITEM',
+    payload: id
+})
